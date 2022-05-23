437f00235ac545df410f75d98cbadadd3719e694
Isis Ribeiro <isisbeatrizrn@gmail.com>
2022-05-23 19:51:40 -0300

    feat create wishlist schema

---
+++ src/models/wishilist-models.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+    
+    title: {
+        type: String, 
+        required: true,
+        trim: true,
+        unique: true
+    },
+    description:{
+        type: String, 
+        required: true,
+        trim: true
+    },
+    client: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Client',
+        required: true,
+        immutable: true
+    },
+    products: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'Produto',
+        required: true,
+        unique: true,
+        trim: true,
+        validate: v => v == null || v.length > 0
+    },
+    createAd: {
+        type: Date,
+        default: Date.now
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
+})
+
+module.exports = mongoose.model('WishList', schema)
