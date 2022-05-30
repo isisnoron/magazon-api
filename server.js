a72fa54de4a287c216344b61d96a210bb1f22743
lilianleandro <lilianleeandro@gmail.com>
2022-05-29 23:24:50 -0300

    procfile

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ import bodyParser from 'body-parser'
 import router from './src/routes'
 import mongoose from "mongoose"
 import env from './src/config/env'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors({
+    origin: '*'
+}));
 app.use(bodyParser.json())
-
 app.use(
     '/swagger',
     swaggerUi.serve, 
