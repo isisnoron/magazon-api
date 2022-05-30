ddd0a9919a38f8aadf6d710443b8d94b447ee2aa
lilianleandro <lilianleeandro@gmail.com>
2022-05-29 23:36:23 -0300

    procfile

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/', router)
 
 mongoose.connect(env.MONGO_DB_URI).then(() => {
 
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
         console.log('Executando na porta 3000...')
     })
 
